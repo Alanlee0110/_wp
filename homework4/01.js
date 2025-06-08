e9b61cc293d57742f4c1e51f1a3c1689eb187375
Alanlee0110 <alanlee950110@gmail.com>
2025-06-08 17:22:18 +0800

    Update 01.js

--- homework4/01.js
+++ homework4/01.js
@@ -1,1 +1,16 @@
+function countLetters(str) {
+    let letterMap = new Map();
 
+    for (let char of str) {
+        if (char.match(/[a-zA-Z]/)) { 
+            char = char.toLowerCase();
+            letterMap.set(char, (letterMap.get(char) || 0) + 1);
+        }
+    }
+
+    return letterMap;
+}
+
+
+let result = countLetters("Hello World!");
+console.log(result);
